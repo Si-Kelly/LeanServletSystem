a208d42897aefc9be8da7e28d2336af1ded20ad3
Si-Kelly <si.kelly.work@gmail.com>
2014-10-08 11:06:43 +0100

    Removed annotations from ValidationExceptionServlet Checked in pom and deployment script

--- lean-servlet-system/src/org/leanservlet/ValidationExceptionServlet.java
+++ lean-servlet-system/src/org/leanservlet/ValidationExceptionServlet.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,11 +12,9 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.leanservlet.bind.ValidationException;
 
-
 /**
  * Servlet implementation class ValidationExceptionServlet
  */
-@WebServlet("/validation-error")
 public class ValidationExceptionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final Log log = LogFactory.getLog(ValidationExceptionServlet.class);
